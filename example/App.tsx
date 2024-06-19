65dd8b938e190e32b6111f5287d4946e19659c00
firstChairCoder <md1qq7@gmail.com>
2024-06-19 20:36:55 +0100

    feat: complete iOS module

--- example/App.tsx
+++ example/App.tsx
@@ -1,20 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-import * as ExpoSettings from 'expo-settings';
+import * as Settings from "expo-settings";
+import { useEffect, useState } from "react";
+import { Button, Text, View } from "react-native";
 
 export default function App() {
+  // const theme = Settings.getTheme();
+  const [theme, setTheme] = useState<string>(Settings.getTheme());
+
+  useEffect(() => {
+    const subscription = Settings.addThemeListener(({ theme: newTheme }) => {
+      setTheme(newTheme);
+    });
+
+    return () => subscription.remove();
+  }, [setTheme]);
+
+  //LOGGER
+  console.log(theme);
+
+  const nextTheme = theme === "dark" ? "light" : "dark";
   return (
-    <View style={styles.container}>
-      <Text>{ExpoSettings.hello()}</Text>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: theme === "dark" ? "#262834" : "seashell",
+      }}
+    >
+      <Text style={{ color: theme === "dark" ? "#FFF" : "#191921" }}>
+        Theme: {Settings.getTheme()}
+      </Text>
+      <Button
+        title={`Set theme to ${nextTheme}`}
+        onPress={() => Settings.setTheme(nextTheme)}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
